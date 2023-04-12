0dc227dcc0f9e31bba2cb10ca029216719d40c0a
kemenovalexey <kemenov.alexeykemenov@yandex.ru>
2023-04-12 14:17:34 +0300

    Задачи на условия

---
+++ 3. Условные конструкции/Задачи/Ход коня.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if abs(a - c) == 1 and abs(b - d) == 2 or abs(b - d) == 1 and abs(a - c) == 2:
+    print("yes")
+else:
+    print("no")
